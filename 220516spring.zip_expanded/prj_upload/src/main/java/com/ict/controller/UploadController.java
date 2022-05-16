580f19bc1079e735405c4a00fda7cb2418374a50
Nogel7 <nogel77@gmail.com>
2022-05-16 18:27:47 +0900

    spring

---
+++ 220516spring.zip_expanded/prj_upload/src/main/java/com/ict/controller/UploadController.java
@@ -0,0 +1,165 @@
+package com.ict.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
+
+@Controller
+@Log4j //pom.xml에서 junit 4.12로, log4j는 1.2.17버전, exclusions 태그 삭제, scope 주석
+public class UploadController {
+	
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+	
+		return str.replace("-", File.separator);
+	}
+	
+
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void upladFormPost(MultipartFile[] uploadFile, Model model) {
+		
+		// 어떤 폴더에 저장할것인지 위치지정
+		String uploadFolder = "C:\\upload_data\\temp";
+		
+		// 폴더 생성 로직
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path : " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		
+		for(MultipartFile multipartFile : uploadFile) {
+			log.info("--------------");
+			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size : " + multipartFile.getSize());
+			
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("last file name : " + uploadFileName);
+			
+			// uuid 발급 부분
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			
+			//File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			
+			
+			try {
+				File saveFile = new File(uploadPath, uploadFileName);
+				multipartFile.transferTo(saveFile);
+				
+				// 이 아래부터 썸네일 생성로직
+				if(checkImageType(saveFile)) {
+					FileOutputStream thumbnail =
+						new FileOutputStream(new File(uploadPath, "s_"+uploadFileName));
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
+				
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}// end for
+	}
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+
+		log.info("upload ajax");
+	}
+	
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		log.info("ajax post update!");
+
+		String uploadFolder = "C:\\upload_data\\temp";
+		
+		// 폴더 생성 로직
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path : " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		
+		for(MultipartFile multipartFile : uploadFile) {
+			log.info("==============================");
+			log.info("upload file name : " + multipartFile.getOriginalFilename());
+			log.info("Upload file size : " + multipartFile.getSize());
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("last file name : " + uploadFileName);
+			
+			// uuid 발급 부분
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//File saveFile = new File(uploadFolder, uploadFileName);
+			
+			
+			try {
+				File saveFile = new File(uploadPath, uploadFileName);
+				multipartFile.transferTo(saveFile);
+				
+				// 이 아래부터 썸네일 생성로직
+				if(checkImageType(saveFile)) {
+					FileOutputStream thumbnail =
+						new FileOutputStream(new File(uploadPath, "s_"+uploadFileName));
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
+				
+				
+			} catch(Exception e) {
+				log.error(e.getMessage());
+			}
+		}
+	}
+	
+	
+	
+}
